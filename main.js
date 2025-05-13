e763db31cc6f9bcd8781414dd03b3427876ab63f
Sang's GIT <sangjp.22@gmail.com>
2025-05-14 00:47:00 +0900

    test

---
+++ main.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('.fade-in');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, { threshold: 0.1 });
+
+sections.forEach(section => observer.observe(section));
